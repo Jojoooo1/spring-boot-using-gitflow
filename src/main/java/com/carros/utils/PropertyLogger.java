b104cd2fdd90e0b90f57868355f7a1d5c3e0c946
Jojoooo1 <chevalier.jonathan@live.fr>
2020-04-13 16:55:22 -0300

    wip

--- src/main/java/com/carros/utils/PropertyLogger.java
+++ src/main/java/com/carros/utils/PropertyLogger.java
@@ -1,36 +1,36 @@
-package com.carros.utils;
+// package com.carros.utils;
 
-import java.util.Arrays;
-import java.util.stream.StreamSupport;
+// import java.util.Arrays;
+// import java.util.stream.StreamSupport;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.context.event.ContextRefreshedEvent;
-import org.springframework.context.event.EventListener;
-import org.springframework.core.env.AbstractEnvironment;
-import org.springframework.core.env.EnumerablePropertySource;
-import org.springframework.core.env.Environment;
-import org.springframework.core.env.MutablePropertySources;
-import org.springframework.stereotype.Component;
+// import org.slf4j.Logger;
+// import org.slf4j.LoggerFactory;
+// import org.springframework.context.event.ContextRefreshedEvent;
+// import org.springframework.context.event.EventListener;
+// import org.springframework.core.env.AbstractEnvironment;
+// import org.springframework.core.env.EnumerablePropertySource;
+// import org.springframework.core.env.Environment;
+// import org.springframework.core.env.MutablePropertySources;
+// import org.springframework.stereotype.Component;
 
-@Component
-public class PropertyLogger {
+// @Component
+// public class PropertyLogger {
 
-  private static final Logger LOGGER = LoggerFactory.getLogger(PropertyLogger.class);
+//   private static final Logger LOGGER = LoggerFactory.getLogger(PropertyLogger.class);
 
-  @EventListener
-  public void handleContextRefresh(ContextRefreshedEvent event) {
-    final Environment env = event.getApplicationContext().getEnvironment();
-    LOGGER.info("====== Environment and configuration ======");
-    LOGGER.info("Active profiles: {}", Arrays.toString(env.getActiveProfiles()));
+//   @EventListener
+//   public void handleContextRefresh(ContextRefreshedEvent event) {
+//     final Environment env = event.getApplicationContext().getEnvironment();
+//     LOGGER.info("====== Environment and configuration ======");
+//     LOGGER.info("Active profiles: {}", Arrays.toString(env.getActiveProfiles()));
 
-    final MutablePropertySources sources = ((AbstractEnvironment) env).getPropertySources();
-    StreamSupport.stream(sources.spliterator(), false).filter(ps -> ps instanceof EnumerablePropertySource)
-        .map(ps -> ((EnumerablePropertySource<?>) ps).getPropertyNames()).flatMap(Arrays::stream).distinct()
-        .filter(prop -> !(prop.contains("java.class.path") || prop.contains("surefire.test.class.path")
-            || prop.contains("credentials") || prop.contains("password")))
-        .sorted().forEach(prop -> LOGGER.info("{}: {}", prop, env.getProperty(prop)));
+//     final MutablePropertySources sources = ((AbstractEnvironment) env).getPropertySources();
+//     StreamSupport.stream(sources.spliterator(), false).filter(ps -> ps instanceof EnumerablePropertySource)
+//         .map(ps -> ((EnumerablePropertySource<?>) ps).getPropertyNames()).flatMap(Arrays::stream).distinct()
+//         .filter(prop -> !(prop.contains("java.class.path") || prop.contains("surefire.test.class.path")
+//             || prop.contains("credentials") || prop.contains("password")))
+//         .sorted().forEach(prop -> LOGGER.info("{}: {}", prop, env.getProperty(prop)));
 
-    LOGGER.info("===========================================");
-  }
-}
+//     LOGGER.info("===========================================");
+//   }
+// }
