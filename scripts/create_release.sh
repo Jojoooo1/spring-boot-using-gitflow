cf0efbd84bfea57413c6968e929556930ac71b0b
Jojoooo1 <chevalier.jonathan@live.fr>
2020-04-13 18:59:25 -0300

    created script

---
+++ scripts/create_release.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# Make sure we are on branch develop
+git checkout develop
+# Get latest tag
+LATEST_TAG=$(git describe --tags $(git rev-list --tags --max-count=1))
+echo
+echo "################################"
+echo "##  Starting release creation ##"
+echo "################################"
+echo
+read -r -p "Enter the release version you want to create (latest: $LATEST_TAG):  " NEW_RELEASE
+echo
+read -r -p "Are you sure you want to create the release \"$NEW_RELEASE\" [Y/n]:  " RESPONSE
+
+if [[ $RESPONSE =~ ^([yY][eE][sS]|[yY])$ ]]; then
+  echo
+  echo "Creating branch $NEW_RELEASE..."
+  git checkout -b $NEW_RELEASE
+  git push origin $NEW_RELEASE
+else
+  echo
+  echo "Exiting..."
+  exit 0
+fi
+
+# validate release
+# create branch release-vX.X
--- scripts/init_project.sh
+++ scripts/init_project.sh
@@ -1,3 +1,12 @@
-# Push to master
-# Create tag
+#!/bin/bash
+
+# Push project to master
+git checkout master
+git add .
+git checkout -m "first commit"
+git push origin master
+# Create and push tag
+git tag -a v1.0.0 -m "First release"
+git push origin v1.0.0
 # Create Develop branch
+git checkout -b develop
