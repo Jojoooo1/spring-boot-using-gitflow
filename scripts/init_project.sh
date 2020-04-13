c2db6b98f5c98d0cdb30d0ee76b552c550aa50d5
Jojoooo1 <chevalier.jonathan@live.fr>
2020-04-13 19:03:02 -0300

    created script

--- scripts/init_project.sh
+++ scripts/init_project.sh
@@ -6,5 +6,5 @@ git push origin master
 # Create and push tag
 git tag -a v1.0.0 -m "First release"
 git push origin v1.0.0
-# Create Develop branch
+# Create develop branch from master
 git checkout -b develop
