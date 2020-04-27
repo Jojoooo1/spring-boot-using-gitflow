02b3f9ff77850eb0a972571e14c6ee4627f45f30
Jojoooo1 <chevalier.jonathan@live.fr>
2020-04-27 14:52:26 -0300

    add readme

--- scripts/create_release.sh
+++ scripts/create_release.sh
@@ -5,8 +5,10 @@ set -e # exit on first error
 # Make sure we are on branch develop
 git checkout develop
 git pull origin develop
+
 # Get latest tag
 LATEST_TAG=$(git describe --tags $(git rev-list --tags --max-count=1))
+
 echo
 echo "################################"
 echo "##  Starting release creation ##"
@@ -17,16 +19,22 @@ echo
 read -r -p "Are you sure you want to create the release \"$NEW_RELEASE\" [Y/n]:  " RESPONSE
 
 if [[ $RESPONSE =~ ^([yY][eE][sS]|[yY])$ ]]; then
+
   BRANCH_NAME="release-$NEW_RELEASE"
+
   echo
   echo "Creating branch $BRANCH_NAME..."
   echo
-  git checkout -b $BRANCH_NAME
+
+  git checkout -b $BRANCH_NAME develop
   git push origin $BRANCH_NAME
+
 else
+
   echo
   echo "Action cancelled exiting..."
   exit 0
+
 fi
 
 # validate release
