a8959288851fdebe611249098976af0843e52557
jonathan <chevalier.jonathan@live.fr>
2020-06-13 16:06:50 -0300

    skaffold

--- skaffold/start.sh
+++ skaffold/start.sh
@@ -11,11 +11,10 @@ current_dir=$PWD
 
 # Sets minikube local daemon
 eval $(minikube docker-env)
-# Sets kubectl namespace
-sudo kubectl config set-context minikube --namespace java-demo
-#
+
 cd ..
+
 # Start skaffold
 skaffold dev --port-forward \
   --verbosity=info --tail=true \
-  --filename=skaffold.yaml
+  --filename=./skaffold/skaffold.yaml
